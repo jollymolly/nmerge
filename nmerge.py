de44612adad2de079cb37fc7e14a3a7694534838
Oleksandr Saienko <sajenko@gmail.com>
2019-02-23 15:39:36 +0200

    Handle empty generators (map masking StopIteration) explicitly

--- nmerge.py
+++ nmerge.py
@@ -1,12 +1,20 @@
 #!python3
-
 """Merge arbitrary sorted iterables"""
 
 
 def nmerge(*args):
 
     data_sources = list(map(iter, filter(bool, args)))
-    data_items = list(map(next, data_sources))
+
+    data_items = []
+    data_source_idx = 0
+    while data_source_idx < len(data_sources):
+        try:
+            data_items.append(next(data_sources[data_source_idx]))
+        except StopIteration:
+            data_sources.pop(data_source_idx)
+            continue
+        data_source_idx += 1
 
     while data_items:
 
@@ -21,7 +29,6 @@ def nmerge(*args):
         except StopIteration:
             data_items.pop(min_idx)
             data_sources.pop(min_idx)
-            
 
 if __name__ == "__main__":
-    print(list(nmerge((7, 8, 9), [5, 6], (), range(0, 5), (v for v in ()))))
+    print(list(nmerge((7, 8, 9), [5, 5, 5, 5, 5, 6], (), range(0, 5), (v for v in ()))))
