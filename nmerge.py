dab447ad92c313dd1f73377eed6915f2a9566d6f
Oleksandr Saienko <sajenko@gmail.com>
2019-02-23 16:03:58 +0200

    get value(s) from data_sources through an interface function

--- nmerge.py
+++ nmerge.py
@@ -2,19 +2,36 @@
 """Merge arbitrary sorted iterables"""
 
 
+def data_sources_get(data_sources, idx = -1):
+
+    data_sources_len = len(data_sources)
+    boundary = idx+1
+
+    if idx == -1:
+        idx = 0
+        boundary = data_sources_len
+    elif not (0 <= idx < data_sources_len):
+        raise IndexError(f"Expected data source index within [0, {data_sources_len}) bounds, got: {idx}.")
+    else:
+        boundary = idx + 1
+
+    ret_val = []
+
+    while idx < boundary:
+        try:
+            ret_val.append(next(data_sources[idx]))
+        except StopIteration:
+            data_sources.pop(idx)
+            boundary -= 1
+            continue
+        idx += 1
+
+    return ret_val
+
 def nmerge(*args):
 
     data_sources = list(map(iter, filter(bool, args)))
-
-    data_items = []
-    data_source_idx = 0
-    while data_source_idx < len(data_sources):
-        try:
-            data_items.append(next(data_sources[data_source_idx]))
-        except StopIteration:
-            data_sources.pop(data_source_idx)
-            continue
-        data_source_idx += 1
+    data_items = data_sources_get(data_sources)
 
     while data_items:
 
@@ -23,12 +40,13 @@ def nmerge(*args):
         yield min_value
 
         min_idx = data_items.index(min_value)
+        next_value = data_sources_get(data_sources, min_idx)
 
-        try:
-            data_items[min_idx] = next(data_sources[min_idx])
-        except StopIteration:
+        if next_value:
+            data_items[min_idx] = next_value[0]
+        else:
             data_items.pop(min_idx)
-            data_sources.pop(min_idx)
+
 
 if __name__ == "__main__":
     print(list(nmerge((7, 8, 9), [5, 5, 5, 5, 5, 6], (), range(0, 5), (v for v in ()))))
